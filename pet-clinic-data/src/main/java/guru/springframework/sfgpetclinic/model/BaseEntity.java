ecc959cd6188deccc8c31d233c3ab6b7690091f4
Veranicus <polacek.patrik@gmail.com>
2019-06-27 12:41:41 +0200

    Iplemented base jpa entity. Closes #19

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by jt on 7/18/18.
  */
+@MappedSuperclass
+@Data
 public class BaseEntity implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
