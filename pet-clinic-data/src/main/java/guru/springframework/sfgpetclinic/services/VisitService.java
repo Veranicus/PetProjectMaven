092c3d6d96166ed63dad7b4ba9b1259cdcc6472f
Veranicus <polacek.patrik@gmail.com>
2019-07-02 12:31:04 +0200

    Implemented SPring Data Jpa service for owners. Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VisitService extends CrudService<Visit, Long> {
+}
