b42ac3772ea261c275250ccdeb7ac302054d931c
Veranicus <polacek.patrik@gmail.com>
2019-07-02 11:24:07 +0200

    Implemented base vet jpa entity. Closes #23

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends CrudRepository<PetRepository, Long> {
+}
